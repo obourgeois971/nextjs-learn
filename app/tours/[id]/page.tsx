ef48b80a6ccb53053600ef0b2bef35ee77dcf9ce
Olivier Bourgeois <bourgeoisolivier@hotmail.fr>
2024-08-14 11:54:33 +0200

    Local Image

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,9 +1,29 @@
+import mapsImg from '@/images/maps.jpg';
+import Image from 'next/image';
+
+console.log(mapsImg);
+
 const page = ({ params }: { params: { id: string } }) => {
   console.log(params);
   // //localhost:3000/tours/123
   http: return (
     <div>
       <h1 className='text-4xl'>ID : {params.id}</h1>
+      <section className='flex gap-x-4 mt-4'>
+        {/* local image */}
+        <div>
+          <Image
+            src={mapsImg}
+            alt='maps'
+            width={192}
+            height={192}
+            className='w-48 h-48 object-cover rounded'
+          />
+          <h2>local image</h2>
+        </div>
+        {/* remote image */}
+        <div></div>
+      </section>
     </div>
   );
 };
