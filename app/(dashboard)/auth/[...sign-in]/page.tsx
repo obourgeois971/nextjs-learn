7416b82eaa7584f42f827ed6fffcaf949cf880cf
Olivier Bourgeois <bourgeoisolivier@hotmail.fr>
2024-08-14 12:53:09 +0200

    Move Routing - Info

---
+++ app/(dashboard)/auth/[...sign-in]/page.tsx
@@ -0,0 +1,7 @@
+// // http//localhost:3000/auth/sign-in
+
+const SignInPage = ({ params }: { params: { 'sign-in': string[] } }) => {
+  console.log(params);
+  return <div>SignInPage</div>;
+};
+export default SignInPage;
---
+++ app/(dashboard)/auth/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AuthPage() {
+  return <h1 className='text-3xl'>AuthPage</h1>;
+}
+
+export default AuthPage;
