2479ee9347b9933c2197fc3abd9d1b06a3a43f78
Olivier Bourgeois <bourgeoisolivier@hotmail.fr>
2024-08-14 09:25:26 +0200

    Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,18 @@
 import './globals.css';
 import Navbar from '@/components/Navbar';
+import { Metadata } from 'next';
 import { Inter, Inconsolata, Roboto } from 'next/font/google';
+import { title } from 'process';
 
 const inter = Inter({ subsets: ['latin'] });
 const inconsolata = Inconsolata({ subsets: ['latin'] });
 const roboto = Roboto({ subsets: ['latin'], weight: ['400'] });
+
+export const metadata: Metadata = {
+  title: 'Next.js Project',
+  description: 'A Next.js project with TypeScript and TailwindCSS.',
+  keywords: 'Next.js, Typescript, TailwindCSS',
+};
 
 export default function RootLayout({
   children,
